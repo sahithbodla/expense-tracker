23034f08135bae9386096d6aa69da77025b31b56
Sahith Bodla <sahith.bodla@gmail.com>
2022-02-13 16:59:37 +0530

    ADD: Context API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import React from 'react';
+
 import Header from './Components/Header';
 import Balance from './Components/Balance';
 import IncomeExpenses from './Components/IncomeExpenses';
 import TransactionList from './Components/TransactionList';
 import AddTransaction from './Components/AddTransaction';
 
+import {GlobalProvider} from "./Context/GlobalState"
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header/>
       <div className="container">
         <Balance/>
@@ -16,7 +19,7 @@ function App() {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
---
+++ src/Context/GlobalState.js
@@ -0,0 +1,23 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+// Initial State
+const initialState = {
+    transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
+  ]
+}
+
+// Creating a context
+export const GlobalContext = createContext(initialState);
+
+// Provider Component - to make store accessible for the components in our React
+export const GlobalProvider = ({children}) => {
+    const [state,dispatch] = useReducer(AppReducer,initialState);
+    return (<GlobalContext.Provider value={{transactions: state.transactions}}>
+        {children}
+    </GlobalContext.Provider>);
+}
