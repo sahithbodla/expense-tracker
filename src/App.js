6790669086f0a75ef86f5dab0d32b69b635604b0
Sahith Bodla <sahith.bodla@gmail.com>
2022-02-13 16:43:19 +0530

    ADD: All Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-import Header from './Components/Header'
+import React from 'react';
+import Header from './Components/Header';
+import Balance from './Components/Balance';
+import IncomeExpenses from './Components/IncomeExpenses';
+import TransactionList from './Components/TransactionList';
+import AddTransaction from './Components/AddTransaction';
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
